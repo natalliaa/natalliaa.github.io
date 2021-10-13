9a2d8759ed792e6306a28e9b7b6e25b21f7d8047
Natallia Akulenka <58709116+NatalliaA@users.noreply.github.com>
2021-10-13 16:18:39 -0600

    Increase font and decrease the divider

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -6,8 +6,8 @@ const { image, name, profession, message } = introDetails;
 
 const IntroSection = () => (
     <section id="intro">
-         <h2 className="my-name">{name}</h2>
-                <h3 className="my-profession">{profession}</h3>
+         <h1 className="my-name">{name}</h1>
+                <h2 className="my-profession">{profession}</h2>
         <Image src={image} alt={name} roundedCircle fluid className="intro-image"/>    
         <p className="intro-message">{message}</p>
     </section>
